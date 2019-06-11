dde3d7310c57960ee016e2b0865a42198d9370ca
Onur Aslan <aslanon.tr@gmail.com>
2019-06-11 15:35:56 +0300

    vue disabled

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import VuejsHeatmap from './VuejsHeatmap'
-export default VuejsHeatmap
+import VuejsHeatmap from "./VuejsHeatmap";
+export default VuejsHeatmap;
 
-import Vue from 'vue'
-Vue.config.productionTip = false
+import Vue from "vue";
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#root',
-  methods: {
-    test: function (e) {
-      console.log('test', e);
-    }
-  },
-  components: { 
-    VuejsHeatmap
-  }
-})
+// new Vue({
+//   el: '#root',
+//   methods: {
+//     test: function (e) {
+//       console.log('test', e);
+//     }
+//   },
+//   components: {
+//     VuejsHeatmap
+//   }
+// })
